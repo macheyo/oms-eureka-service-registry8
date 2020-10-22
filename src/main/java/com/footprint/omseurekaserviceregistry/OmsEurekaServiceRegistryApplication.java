1badb3ebb52e6da43f23852062c088315d9621f7
kudzai.macheyo <kudzai.macheyo@cassavasmartech.co.zw>
2020-10-22 13:53:10 +0200

    Initial commit

---
+++ src/main/java/com/footprint/omseurekaserviceregistry/OmsEurekaServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.footprint.omseurekaserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmsEurekaServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmsEurekaServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footprint/omseurekaserviceregistry/OmsEurekaServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footprint.omseurekaserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsEurekaServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
